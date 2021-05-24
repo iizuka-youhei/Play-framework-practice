ec61e9e09877582e4ccd15cb5fba079a07a60ee7
iizuka-youhei <y-iizuka@croooober.co.jp>
2021-05-24 18:33:16 +0900

    コントローラーのテストコードの作成

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import models.UserEntity;
+import models.MicropostEntity;
+
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 import play.Application;
@@ -8,9 +11,9 @@ import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
 import play.test.Helpers;
+import io.ebean.*;
 
 import static org.junit.Assert.assertEquals;
-// import static play.mvc.Http.Status.OK;
 import static play.mvc.Http.Status.*;
 import static play.test.Helpers.*;
 import static play.test.Helpers.route;
@@ -63,35 +66,68 @@ public class HomeControllerTest extends WithApplication {
         assertEquals("/", result.redirectLocation().get());
     }
 
-    // @Test
-    // public void 自分の投稿ならば編集ページを表示できる() {
-    //     Http.RequestBuilder request = Helpers.fakeRequest()
-    //             .method(GET)
-    //             .uri("/edit/1");
-        
-    //     request.session("login", Ebean.find(UserEntity.class, 1)); 
-        
-    //     Result result = route(app, request);
-    //     assertEquals(OK, result.status());
-    // }
-
     @Test
-    public void ログインしていないと投稿できない() {
+    public void 掲示板に投稿できるか() {
         Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(POST)
                 .bodyForm(ImmutableMap.of("title", "タイトルです", "message", "メッセージです。", "link", ""))
                 .uri("/create");
+
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail());
+        int post_num = Ebean.find(MicropostEntity.class).findList().size();
         
         Result result = route(app, request);
         assertEquals(SEE_OTHER, result.status());
         assertEquals("/", result.redirectLocation().get());
+        assertEquals(post_num + 1, Ebean.find(MicropostEntity.class).findList().size());
+    }
+
+    @Test
+    public void ログインしていないと掲示板に投稿できない() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .bodyForm(ImmutableMap.of("title", "タイトルです", "message", "メッセージです。", "link", ""))
+                .uri("/create");
+
+        int post_num = Ebean.find(MicropostEntity.class).findList().size();
+        
+        Result result = route(app, request);
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/", result.redirectLocation().get());
+        assertEquals(post_num, Ebean.find(MicropostEntity.class).findList().size());
+    }
+
+    @Test
+    public void 自分の投稿ならば編集ページを表示できる() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(GET)
+                .uri("/edit/1");
+        
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail()); 
+        
+        Result result = route(app, request);
+        assertEquals(OK, result.status());
     }
 
     @Test
     public void 自分の投稿でないと編集ページを表示できない() {
         Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(GET)
-                .uri("/edit/1000000");
+                .uri("/edit/2");
+        
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail()); 
+        
+        Result result = route(app, request);
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/", result.redirectLocation().get());
+    }
+
+
+    @Test
+    public void ログインしていないと編集ページを表示できない() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(GET)
+                .uri("/edit/1");
         
         Result result = route(app, request);
         assertEquals(SEE_OTHER, result.status());
@@ -99,14 +135,107 @@ public class HomeControllerTest extends WithApplication {
     }
 
     @Test
+    public void 自分の投稿なら編集ができる() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .bodyForm(ImmutableMap.of("title", "編集後のタイトルです", "message", "編集後のメッセージです", "link", ""))
+                .uri("/update/1");
+
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail());
+        int post_num = Ebean.find(MicropostEntity.class).findList().size();
+        
+        Result result = route(app, request);
+        MicropostEntity updated_post = Ebean.find(MicropostEntity.class, 1);
+
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/", result.redirectLocation().get());
+        assertEquals(post_num, Ebean.find(MicropostEntity.class).findList().size());
+        assertEquals("編集後のタイトルです", updated_post.getTitle());
+        assertEquals("編集後のメッセージです", updated_post.getMessage());
+    }
+
+    @Test
+    public void 他人の投稿は編集できない() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .bodyForm(ImmutableMap.of("title", "タイトルです", "message", "メッセージです。", "link", ""))
+                .uri("/update/2");
+
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail());
+        int post_num = Ebean.find(MicropostEntity.class).findList().size();
+        MicropostEntity post = Ebean.find(MicropostEntity.class, 2);
+        
+        Result result = route(app, request);
+        MicropostEntity updated_post = Ebean.find(MicropostEntity.class, 2);
+
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/", result.redirectLocation().get());
+        assertEquals(post_num, Ebean.find(MicropostEntity.class).findList().size());
+        assertEquals(post.getTitle(), updated_post.getTitle());
+        assertEquals(post.getMessage(), updated_post.getMessage());
+    }
+
+    @Test
+    public void 自分の投稿ならば削除ページを表示できる() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(GET)
+                .uri("/delete/1");
+
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail()); 
+        
+        Result result = route(app, request);
+        assertEquals(OK, result.status());
+    }
+
+    @Test
     public void 自分の投稿でないと削除ページを表示できない() {
         Http.RequestBuilder request = Helpers.fakeRequest()
                 .method(GET)
-                .uri("/delete/1000000");
+                .uri("/delete/2");
+
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail()); 
         
         Result result = route(app, request);
         assertEquals(SEE_OTHER, result.status());
         assertEquals("/", result.redirectLocation().get());
+    }
+
+    @Test
+    public void ログインしていないと削除ページを表示できない() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(GET)
+                .uri("/delete/1");
+        
+        Result result = route(app, request);
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/", result.redirectLocation().get());
+    }
+
+    @Test
+    public void 自分の投稿は削除できる() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .uri("/remove/1");
+        
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail());
+
+        Result result = route(app, request);
+        assertEquals("/", result.redirectLocation().get());
+        assertEquals(null, Ebean.find(MicropostEntity.class, 1));
+    }
+
+    @Test
+    public void 他人の投稿は削除できない() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .uri("/remove/2");
+        
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail()); 
+        MicropostEntity post = Ebean.find(MicropostEntity.class, 2);
+
+        Result result = route(app, request);
+        assertEquals("/", result.redirectLocation().get());
+        assertEquals(post, Ebean.find(MicropostEntity.class, 2));
     }
 
     @Test
--- test/controllers/UserControllerTest.java
+++ test/controllers/UserControllerTest.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import models.UserEntity;
+
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
@@ -7,11 +10,11 @@ import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
 import play.test.Helpers;
+import io.ebean.*;
 
 import static org.junit.Assert.assertEquals;
-// import static play.mvc.Http.Status.OK;
 import static play.mvc.Http.Status.*;
-import static play.test.Helpers.GET;
+import static play.test.Helpers.*;
 import static play.test.Helpers.route;
 
 
@@ -31,6 +34,57 @@ public class UserControllerTest extends WithApplication {
         
         Result result = route(app, request);
         assertEquals(OK, result.status());
+    }
+
+    @Test
+    public void ログイン中はloginページを表示できない() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(GET)
+                .uri("/login");
+
+        request.session("login", Ebean.find(UserEntity.class, 1).getEmail()); 
+        
+        Result result = route(app, request);
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/", result.redirectLocation().get());
+    }
+
+    @Test
+    public void ログインしたらトップページにリダイレクトする() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .bodyForm(ImmutableMap.of("email", "sato@gmail.com", "password", "sato123"))
+                .uri("/logincheck");
+
+        // UserEntity user = Ebean.find(UserEntity.class, 2);
+        Result result = route(app, request);
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/", result.redirectLocation().get());
+    }
+
+    @Test
+    public void 間違ったメールアドレスやパスワードを入力したらloginページへリダイレクトする() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(POST)
+                .bodyForm(ImmutableMap.of("email", "sato@gmail.com", "password", "sato456"))
+                .uri("/logincheck");
+
+        // UserEntity user = Ebean.find(UserEntity.class, 2);
+        Result result = route(app, request);
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/login", result.redirectLocation().get());
+    }
+
+    @Test
+    public void ログアウトしたらトップページにリダイレクトする() {
+        Http.RequestBuilder request = Helpers.fakeRequest()
+                .method(GET)
+                .bodyForm(ImmutableMap.of("email", "sato@gmail.com", "password", "sato123"))
+                .uri("/logout");
+
+        Result result = route(app, request);
+        assertEquals(SEE_OTHER, result.status());
+        assertEquals("/", result.redirectLocation().get());
     }
 
 }
