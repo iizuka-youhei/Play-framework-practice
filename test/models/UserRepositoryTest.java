c6d60c681f51e0ded718ee7637d078dc44ff98d7
iizuka-youhei <y-iizuka@croooober.co.jp>
2021-05-27 18:22:51 +0900

    20210527途中のコード

--- test/models/UserRepositoryTest.java
+++ test/models/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import models.UserEntity;
 import models.MicropostEntity;
 
 import com.google.common.collect.ImmutableMap;
+import org.junit.*;
 import org.junit.Test;
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
@@ -15,8 +16,15 @@ import play.test.Helpers;
 import io.ebean.*;
 import javax.inject.Inject;
 
+import play.db.Database;
+import play.db.Databases;
+import play.db.evolutions.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 import static play.mvc.Http.Status.*;
 import static play.test.Helpers.*;
 import static play.test.Helpers.route;
@@ -32,11 +40,25 @@ public class UserRepositoryTest extends WithApplication {
     }
 
     private final UserRepository repo = new UserRepository();
-
+    Database database;
     // @Inject
     // public UserRepositoryTest(UserRepository userRepository) {
     //     this.repo = userRepository;
     // }
+
+    @Before
+    public void setupDatabase() {
+        database = Databases.inMemory();
+        Evolutions.applyEvolutions(
+            database,
+            Evolutions.fromClassLoader(getClass().getClassLoader(), "testdatabase/"));
+    }
+
+    @After
+    public void shutdownDatabase() {
+        Evolutions.cleanupEvolutions(database);
+        database.shutdown();
+    }
 
     @Test
     public void 全ユーザーを取得できるか() {
